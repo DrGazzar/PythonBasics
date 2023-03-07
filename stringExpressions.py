87f3756f1f8be8c5d3a2bffd5b81685f84616e0e
DrGazzar <mohamedhelgazzar@gmail.com>
2023-03-07 12:50:31 +0200

    Update stringExpressions.py

--- stringExpressions.py
+++ stringExpressions.py
@@ -1,6 +1,6 @@
 #This script shows how to declare string variables and apply different operations on them
 #License GPL
-#Created by: Mohamed ElGazzar
+#Created by: Mohamed ElGazzar (DrGazzar)
 
 frontEndServer = 'Web Server'
 backEndServer = 'DB Server'
@@ -16,3 +16,12 @@ print ('The servers are ', frontEndServer, ' and ', backEndServer)
 frontLength = len(frontEndServer)
 
 print('The length of the front end server is ', frontLength)
+
+#Also there are functions that transforms the strings
+
+# Capitalize the first letter of the string so the output will be       Db server 
+print ( backEndServer.capitalize()  )
+
+#  Converts the whole string to small (lower) letters so the output will be       db server 
+print ( backEndServer.casefold()  )
+
