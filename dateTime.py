ded6255edde62d66c32575762496c24f0dc60fc1
DrGazzar <mohamedhelgazzar@gmail.com>
2023-03-08 19:34:47 +0200

    Create dateTime.py

---
+++ dateTime.py
@@ -0,0 +1,14 @@
+#This script shows how to demonestrate the date time in python
+#License GPL
+#Created by: Mohamed ElGazzar (DrGazzar) 
+#Github repo: https://github.com/DrGazzar/PythonBasics
+
+import datetime
+
+print(datetime.datetime.now())  #date time
+
+print(datetime.date.today())   #date only
+
+print(datetime.datetime.now().year)  #year only
+
+print(datetime.date.today().day)   #day only
