3e428c9bc5faf5ce3917f82324e9c0723be84df7
DrGazzar <mohamedhelgazzar@gmail.com>
2023-03-08 16:25:30 +0200

    Create importModule.py

---
+++ importModule.py
@@ -0,0 +1,13 @@
+#This script shows how to demonestrate how to import modules in Python
+#License GPL
+#Created by: Mohamed ElGazzar (DrGazzar) 
+#Github repo: https://github.com/DrGazzar/PythonBasics
+
+
+import scopes
+# note that when importing a file, it will be executed ... so it should be only functions, classes, etc without calling them
+
+scopes.getType()
+
+
+scopes.getGlobalType()
