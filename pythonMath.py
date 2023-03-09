a46758f6a71047e18a796de39b69b6a0541604ca
DrGazzar <mohamedhelgazzar@gmail.com>
2023-03-09 13:20:36 +0200

    Create pythonMath.py

---
+++ pythonMath.py
@@ -0,0 +1,32 @@
+#This script shows how to demonestrate the math functions in python
+#License GPL
+#Created by: Mohamed ElGazzar (DrGazzar)
+#Github repo: https://github.com/DrGazzar/PythonBasics
+
+
+#Minimum value
+
+firstDegree = 12
+secondDegree = 983
+thirdDegree = -364
+minDegree = min(firstDegree, secondDegree, thirdDegree)
+
+print("The minimum Degree is: ", minDegree)
+
+#Maximum value
+
+maxDegree = max(firstDegree,secondDegree,thirdDegree)
+
+print("The maximum Degree is: ", maxDegree)
+
+#Absolute value
+
+absDegree = abs(minDegree)
+
+print("Absolute valye is: ", absDegree)
+
+#Power
+
+powerValue =pow(firstDegree, 5)
+
+print("The result of the power operation is : ", powerValue)
