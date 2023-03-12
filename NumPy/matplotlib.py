38ac5d4e73b7859fbc414aac5058c46441b79459
DrGazzar <mohamedhelgazzar@gmail.com>
2023-03-12 09:47:59 +0200

    Create matplotlib.py

---
+++ NumPy/matplotlib.py
@@ -0,0 +1,9 @@
+#This script shows how to demonestrate numpy matplotlib
+#License GPL
+#Created by: Mohamed ElGazzar (DrGazzar)
+#Github repo: https://github.com/DrGazzar/PythonBasics
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+sns.distplot(["Java", "Python", "C#"])
