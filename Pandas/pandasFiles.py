bde1e5fdc5744833ee49b7fd719fd037ccbdba1c
DrGazzar <mohamedhelgazzar@gmail.com>
2023-03-14 15:58:06 +0200

    Pandas files

--- Pandas/pandasFiles.py
+++ Pandas/pandasFiles.py
@@ -22,3 +22,46 @@ pd.options.display.max_rows = 59
 
 ##########
 #### Read json file using pandas
+
+serverListFromJson = pd.read_json("Pandas/assets.json")
+
+print("################# from JSON ##############")
+print(serverListFromJson)
+
+
+
+####### Python dictionaries are the same as JSON format
+
+books = {
+
+    "Python Crash Course" : {
+        "Author": "Eric",
+        "Topic" : "Python",
+        "Pages" : 544 
+        
+    },
+
+    "Automate with python" : {
+        "Author": "Ed",
+        "Topic" : "Automation",
+        "Pages" : 200
+        
+    },
+    
+        "Learning python" : {
+        "Author": "Mark",
+        "Topic" : "Python",
+        "Pages" : 600 
+        
+    }
+    
+
+}
+
+
+allBooks = pd.DataFrame(books)
+
+print("---------All books dictionary/json-----\n", allBooks)
+
+
+######
