72de8f68ec32487b1cb67707c78e45003e241328
DrGazzar <mohamedhelgazzar@gmail.com>
2023-03-16 23:48:28 +0200

    Create pythonCheckVersion.py

---
+++ Python 101/pythonCheckVersion.py
@@ -0,0 +1,8 @@
+#This script shows how to check the python version deployed in the system
+#License GPL
+# Created by: Mohamed ElGazzar
+#Github repo: https://github.com/DrGazzar/PythonBasics
+
+
+import sys
+print (sys.version)
