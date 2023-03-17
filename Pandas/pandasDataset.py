7354571390c6d046aee6e1ef0c0fd34bb3a4e623
DrGazzar <mohamedhelgazzar@gmail.com>
2023-03-17 14:20:19 +0200

    Update pandasDataset.py

--- Pandas/pandasDataset.py
+++ Pandas/pandasDataset.py
@@ -79,5 +79,39 @@ print("One row \n ####################\n", studentsDataset.loc[1:1])
 
 print("By label  \n ####################\n", studentsDataset2.loc["Course 1"])
 
-##########
+########## create data frame from list of dictionaries
+
+courseListOfDictionaries = [
+
+{'course_name': 'Networks', 'course_id' : 15},{'course_name': 'Programming', 'course_id': 20},{'course_name': 'cloud computing', 'course_id': 25, 'course_description': 'AWS Cloud Computing'}
+
+]
+
+courseDataFrame = pd.DataFrame(courseListOfDictionaries, index = ['First', 'Second', 'Third'])
+
+print ("\n\nThe course data frame (course list of dictionaries) \n", courseDataFrame)
+
+
+############## create a data frame from a dictionary of series
+
+
+infrastructure =  {
+
+'webServer' : pd.Series(
+    [ 'Company Web Server', 'Apache Web Server' , 'Magnetic'], index = ['Server Name', 'Software', 'Storage'] ),
+
+'databaseServer' : pd.Series(
+    [ 'Company Database Server', 'Mongo DB', 'SSD', 'Secured Netework Zone']  , index = ['Server Name', 'Software', 'Storage', 'Network Zone'])
+
+
+}
+
+infrastructureDataFrame = pd.DataFrame(infrastructure)
+
+print ("\n\nThe infrastructure is \n\n", infrastructureDataFrame)
+### A column can be added too
+
+infrastructureDataFrame['proxy server'] = pd.Series( ['Rewrite rules', 'Apache Proxy', 'Magnetic', 'Frondend'] , index = ['Server Name', 'Software', 'Storage', 'Network Zone'] )
+
+print ("\n\nThe infrastructure - after adding new colum - is  \n\n", infrastructureDataFrame)
 
