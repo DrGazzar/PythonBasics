0a8f9104e67582f729602f168c811433cfccdfc1
DrGazzar <mohamedhelgazzar@gmail.com>
2023-03-19 13:39:22 +0200

    Create mongoDB.py

---
+++ MongoDB/mongoDB.py
@@ -0,0 +1,61 @@
+#This script shows how to connect to MongoDB
+#License GPL
+#Created by: Mohamed ElGazzar (DrGazzar)
+#Github repo: https://github.com/DrGazzar/PythonBasics
+
+
+import pymongo
+
+clientDB = pymongo.MongoClient("mongodb://localhost:27017/")
+
+workingDB = clientDB["workingDB"] 
+
+#### MongoDB does not create the database untill there are some contents inside it
+#### collection includes documents 
+#### Documents are similar to - but not exactly the same as - rows
+#### Documents have to have key value pairs
+#### e.g.  {username: 'a name', address: 'an address'}  --> looks like the python dictionary and JSON files
+
+### creating collection
+### collection is not created until there are some contents in it
+
+workingCollection = workingDB["serverLogs"]
+
+### to insert a dictionary , we could create a dictionary
+
+serverData = {
+    "serverName" : "web server",
+    "serverSoftware" : "Apache Web Server",
+    "Proxy" : "Yes"
+}
+
+insertionData = workingCollection.insert_one(serverData)
+
+print("The ID of the inserted data is: ",insertionData.inserted_id)
+
+### can insert multiple documents at one statement ... using list of dictionaries
+
+allServers = [
+{"serverName" : "Database Server", "serverSoftware": "MongoDB", "Proxy": "N/A"},
+{"serverName" : "Structured Database 1", "serverSoftware": "MySql", "Proxy": "N/A"},
+{"serverName" : "Structured Database 2", "serverSoftware": "Oracle", "Proxy": "N/A"},
+{"serverName" : "Print Server", "serverSoftware": "Samba", "Proxy": "No"},
+{"serverName" : "Mail Server", "serverSoftware": "qmail", "Proxy": "No"}
+
+]
+
+insertionMultipleDocuments = workingCollection.insert_many(allServers)
+
+print("The ID of the inserted documents is: ",insertionMultipleDocuments.inserted_ids)
+
+
+########################################################################
+### check if a database exists
+
+listOfDatabases = clientDB.list_database_names()
+listOfCollections = workingDB.list_collection_names()
+
+print ("List of Databases :", listOfDatabases)
+print ("List of collections :", listOfCollections)
+
+################################
